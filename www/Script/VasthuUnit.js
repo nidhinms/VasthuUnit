f9f0509259f2227e733d96b3e775afbbc1495b6d
nidhinmshani <nidhinmshani@gmail.com>
2015-02-19 21:23:53 +0530

    Conversion Basic Completed

--- www/Script/VasthuUnit.js
+++ www/Script/VasthuUnit.js
@@ -1,5 +1,9 @@
 function VasthuUnit(cm) {
 
+    this.koal = 0;
+    this.viral = 0;
+    this.yava = 0;
+
     if (arguments.length == 1) {
         var reminder;
 
@@ -8,27 +12,19 @@ function VasthuUnit(cm) {
         this.viral = reminder / 3;
         this.yava = reminder / 8;
     }
-    else if (arguments.length == 3)
-    {
+    else if (arguments.length == 3) {
         this.koal = arguments[0];
-        this.viral = arguments[0];
-        this.yava = arguments[0];
-    }
-    else
-    {
-        this.koal = 0;
-        this.viral = 0;
-        this.yava = 0;
+        this.viral = arguments[1];
+        this.yava = arguments[2];
     }
 
     this.updateCMValue = function(cm){
         var reminder;
 
-        this.koal = cm / 72;
+        this.koal = Math.floor(cm / 72);
         reminder = cm % 72;
-        this.viral = reminder / 3;
-        this.yava = reminder / 8;
+        this.viral = Math.floor(reminder / 3);
+        this.yava = Math.floor(reminder / 8);
     }
-
 }
 
