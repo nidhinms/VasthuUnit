91b8a2ca94b98695ce82c41854f6609385dcc5f4
nidhinmshani <nidhinmshani@gmail.com>
2015-02-21 00:26:53 +0530

    Working Prototype

--- www/Script/VasthuUnit.js
+++ www/Script/VasthuUnit.js
@@ -1,30 +1,43 @@
 function VasthuUnit(cm) {
-
     this.koal = 0;
     this.viral = 0;
     this.yava = 0;
 
     if (arguments.length == 1) {
+
         var reminder;
 
         this.koal = cm / 72;
         reminder = cm % 72;
         this.viral = reminder / 3;
-        this.yava = reminder / 8;
+        reminder = reminder % 3;
+        this.yava = reminder / (3 / 8);
+        this.yava = this.yava.toFixed(1);
     }
     else if (arguments.length == 3) {
         this.koal = arguments[0];
         this.viral = arguments[1];
         this.yava = arguments[2];
     }
-
-    this.updateCMValue = function(cm){
+    this.updateCMValue = function (cm) {
         var reminder;
 
         this.koal = Math.floor(cm / 72);
         reminder = cm % 72;
         this.viral = Math.floor(reminder / 3);
-        this.yava = Math.floor(reminder / 8);
+        reminder = reminder % 3;
+        this.yava = reminder / (3 / 8);
+        this.yava = this.yava.toFixed(1);
     }
-}
 
+               
+
+                this.getCM = function() {
+
+                                var cmValue = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
+
+                                return (cmValue.toFixed(2));
+
+                }
+
+}
--- www/Script/events.js
+++ www/Script/events.js
@@ -1,7 +1,7 @@
 function Test() {
     console.log("Testing in progress...");
-    var unit = new VasthuUnit(96);
-    if (unit.koal != 1 && unit.viral != 8 && unit.yava != 3) {
+    var unit = new VasthuUnit(79.5);
+    if (unit.koal != 1 && unit.viral != 2 && unit.yava != 4) {
         alert("Error in conversion from constructor");
     }
     console.log("Testing complete...");
