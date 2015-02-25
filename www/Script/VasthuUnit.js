cd15657f4f214e736c29bee5d3f1f9ad5c1ceb35
nidhinmshani <nidhinmshani@gmail.com>
2015-02-26 00:05:28 +0530

    Changed to angular framework

--- www/Script/VasthuUnit.js
+++ www/Script/VasthuUnit.js
@@ -2,14 +2,17 @@ function VasthuUnit(cm) {
     this.koal = 0;
     this.viral = 0;
     this.yava = 0;
+	this.cm = 0;
 
     if (arguments.length == 1) {
 
         var reminder;
-
-        this.koal = cm / 72;
+		
+		this.cm = cm;
+		
+        this.koal = Math.floor(cm / 72);
         reminder = cm % 72;
-        this.viral = reminder / 3;
+        this.viral = Math.floor(reminder / 3);
         reminder = reminder % 3;
         this.yava = reminder / (3 / 8);
         this.yava = this.yava.toFixed(1);
@@ -18,26 +21,44 @@ function VasthuUnit(cm) {
         this.koal = arguments[0];
         this.viral = arguments[1];
         this.yava = arguments[2];
+		
+		this.cm = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
     }
-    this.updateCMValue = function (cm) {
-        var reminder;
-
-        this.koal = Math.floor(cm / 72);
-        reminder = cm % 72;
-        this.viral = Math.floor(reminder / 3);
+		               
+    this.getCM = function() {
+		cm = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
+		return (cm.toFixed(2));
+	}
+	
+	this.getVasthuUnit = function() {
+		var ret = {};
+		ret.koal = Math.floor(this.cm / 72);
+        var reminder = this.cm % 72;
+        ret.viral = Math.floor(reminder / 3);
         reminder = reminder % 3;
-        this.yava = reminder / (3 / 8);
-        this.yava = this.yava.toFixed(1);
-    }
-
-               
-
-                this.getCM = function() {
-
-                                var cmValue = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
-
-                                return (cmValue.toFixed(2));
-
-                }
-
+        ret.yava = reminder / (3 / 8);
+        ret.yava = ret.yava.toFixed(1);
+		return ret;
+	}
+	
+	this.add = function(val) {
+		this.cm = Math.floor(this.cm + val.cm);
+		this.koal = Math.floor(this.koal + val.koal);
+		this.viral = Math.floor(this.viral + val.viral);
+		this.yava = Math.floor(this.yava + val.yava);
+		this.rearrange();
+	}
+	
+	this.rearrange = function() {
+		if (this.yava >= 8)
+		{
+			this.viral = Math.floor(this.viral + this.yava / 8);
+			this.yava = this.yava % 8;
+		}
+		if (this.viral >= 24)
+		{
+			this.koal = Math.floor(this.koal + this.viral / 24);
+			this.viral = this.viral % 24;
+		}
+	}
 }
--- www/Script/events.js
+++ www/Script/events.js
@@ -2,7 +2,63 @@ function Test() {
     console.log("Testing in progress...");
     var unit = new VasthuUnit(79.5);
     if (unit.koal != 1 && unit.viral != 2 && unit.yava != 4) {
-        alert("Error in conversion from constructor");
+        alert("Error in conversion formula. Incorrect output may be generated");
     }
     console.log("Testing complete...");
 }
+
+var app = angular.module('VasthuUnitApp', []);
+app.controller('convertPageController', ['$scope',function($scope) {
+	$scope.unitObjectForCMToV = new VasthuUnit(0);
+	$scope.unitObjectForVToCM = new VasthuUnit(0);
+	$scope.pageProperties = {};
+	$scope.pageProperties.convertDirection = 1;
+	$scope.convertedFromCM = function() {
+		var value = $scope.unitObjectForCMToV.getVasthuUnit();
+		return value.koal + " koal" + value.viral + " viral" + value.yava + " yava";
+	}
+	$scope.convertedFromVasthuUnit = function() {
+		if ($scope.pageProperties.autoRearrange == true)
+		{
+			$scope.unitObjectForVToCM.rearrange();
+		}
+		return $scope.unitObjectForVToCM.getCM() + "cm";
+	}
+	$scope.getActiveConvertDirectionCSS = function() {
+		if ($scope.pageProperties.convertDirection == 1)
+		{
+			return "ui-btn-active";
+		}
+		else
+		{
+			return "";
+		}
+	}
+	$scope.pageProperties.autoRearrange = false;
+}]);
+app.controller("addScreenController", function($scope) {
+	$scope.additionItems = [];
+	$scope.additionItems[0] = new VasthuUnit(0,0,0);
+	$scope.additionItems[1] = new VasthuUnit(0,0,0);
+	$scope.addNew = function() {
+		$scope.additionItems.push(new VasthuUnit(0,0,0));
+	}
+	$scope.remove = function() {
+		$scope.additionItems.pop();
+	}
+	$scope.getSum = function() {
+		var sum = new VasthuUnit(0,0,0);
+		var unitObject;
+		$("#page_addScreen_itemList").listview().trigger('create');
+		$("#page_addScreen_itemList").listview('refresh');
+		for(unitObject in $scope.additionItems) {
+			$scope.additionItems[unitObject].rearrange();
+			sum.add($scope.additionItems[unitObject]);
+		}
+		return sum;
+	}
+});
+
+$(function() {
+	Test();
+});
