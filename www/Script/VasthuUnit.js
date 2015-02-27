28a655ac5ea8b08b39a1fe51217dbad0db1e4bba
nidhinmshani <nidhinmshani@gmail.com>
2015-02-28 00:01:56 +0530

    Negative Number also considered

--- www/Script/VasthuUnit.js
+++ www/Script/VasthuUnit.js
@@ -4,14 +4,26 @@ function VasthuUnit(cm) {
 	this.yava = 0;
 	this.cm = 0;
 	if (arguments.length == 1) {
-		var reminder;
-		this.cm = cm;
+	    this.cm = arguments[0];
+	    var negative = false;
+        if (cm < 0)
+        {
+            negative = true;
+            cm = cm * -1;
+        }
+        var reminder;
 		this.koal = Math.floor(cm / 72);
 		reminder = cm % 72;
 		this.viral = Math.floor(reminder / 3);
 		reminder = reminder % 3;
 		this.yava = reminder / (3 / 8);
-		this.yava = this.yava.toFixed(1);
+		this.yava = parseFloat(this.yava);
+        if (negative)
+        {
+            this.koal = this.koal * -1;
+            this.viral = this.viral * -1;
+            this.yava = this.yava * -1;
+        }
 	} else if (arguments.length == 3) {
 		this.koal = arguments[0];
 		this.viral = arguments[1];
@@ -19,32 +31,41 @@ function VasthuUnit(cm) {
 		this.cm = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
 	}
 	this.getCM = function() {
-		cm = this.koal * 72 + this.viral * 3 + this.yava * (3 / 8);
-		return (cm.toFixed(2));
+		cm = this.koal * 72.0 + this.viral * 3.0 + this.yava * (3 / 8);
+		return (parseFloat(cm));
 	}
-	this.getVasthuUnit = function() {
-		var ret = {};
-		ret.koal = Math.floor(this.cm / 72);
-		var reminder = this.cm % 72;
-		ret.viral = Math.floor(reminder / 3);
-		reminder = reminder % 3;
-		ret.yava = reminder / (3 / 8);
-		ret.yava = ret.yava.toFixed(1);
-		return ret;
+	this.getVasthuUnit = function () {
+	    if (cm < 0) {
+	        console.log("Reverse");
+	        reverseSign();
+	    }
+	    var k = Math.floor(this.cm / 72);
+	    var reminder = this.cm % 72;
+	    var v = Math.floor(reminder / 3);
+	    reminder = reminder % 3;
+	    var y = reminder / (3 / 8);
+	    y = parseFloat(y).toFixed(2);
+	    var ret = new VasthuUnit(k, v, y);
+	    if (cm < 0) {
+	        ret.reverseSign();
+	    }
+	    return ret;
 	}
 	this.add = function(val) {
 		this.cm = Math.floor(this.cm + val.cm);
-		this.koal = Math.floor(this.koal + val.koal);
-		this.viral = Math.floor(this.viral + val.viral);
-		this.yava = Math.floor(this.yava + val.yava);
+		this.koal = this.koal + val.koal;
+		this.viral = this.viral + val.viral;
+		this.yava = this.yava + val.yava;
 		this.rearrange();
 	}
-	this.subtract = function(val) {
-		this.cm = Math.floor(this.cm - val.cm);
-		this.koal = Math.floor(this.koal - val.koal);
-		this.viral = Math.floor(this.viral - val.viral);
-		this.yava = Math.floor(this.yava - val.yava);
-		this.rearrange();
+	this.subtract = function (val) {
+	    this.cm = Math.floor(this.cm - val.cm);
+	    var sum = this.getCM() - val.getCM();
+
+	    this.koal = this.koal - val.koal;
+	    this.viral = this.viral - val.viral;
+	    this.yava = this.yava - val.yava;
+	    this.rearrange();
 	}
 	this.multiply = function(val) {
 		this.cm = this.cm * val;
@@ -54,46 +75,66 @@ function VasthuUnit(cm) {
 		this.rearrange();
 	}
 	this.divide = function(val) {
-		var r = 0;
 		this.koal = this.koal / val;
-		r = this.koal - Math.floor(this.koal);
-		this.viral = this.viral + r * 72;
-		this.koal = Math.floor(this.koal);
 		this.viral = this.viral / val;
-		r = this.viral - Math.floor(this.viral);
-		this.yava = this.yava + r * (8);
-		this.viral = Math.floor(this.viral);
-		this.yava = (this.yava / val).toFixed(2);
-		if (this.yava >= 8) {
-			this.rearrange();
-		}
+		this.yava = this.yava / val;
+		this.rearrange();
 	}
-	this.rearrange = function() {
-		var integerPart = Math.floor(this.koal);
-		var r = 0;
-		if (this.koal != integerPart) {
-			r = this.koal - integerPart;
-			this.koal = r;
-			this.viral = this.viral + r * 24;
-			integerPart = Math.floor(this.koal);
-			r = this.viral - integerPart;
-			this.viral = r;
-			this.yava = this.yava + r * 8;
-		}
-		if (this.yava >= 8) {
-			this.viral = Math.floor(this.viral + this.yava / 8);
-			this.yava = this.yava % 8;
-			this.yava = this.yava.toFixed(2);
-		}
-		if (this.viral >= 24) {
-			this.koal = Math.floor(this.koal + this.viral / 24);
-			this.viral = this.viral % 24;
-		}
+	this.rearrange = function () {
+	    var negative = this.isNegative();
+	    if (negative) {
+	        reverseSign();
+	    }
+	    var fractionKoal = parseFloat(this.koal % 1);
+	    if (fractionKoal > 0) {
+	        this.koal = Math.floor(this.koal);
+	        this.viral = this.viral + fractionKoal * 24;
+	    }
+	    var fractionViral = parseFloat(this.viral % 1);
+	    if (fractionViral > 0) {
+	        this.viral = Math.floor(this.viral);
+	        this.yava = this.yava + fractionViral * 8;
+	    }
+	    if (this.yava >= 8) {
+	        var viralToAdd = Math.floor(this.yava / 8);
+	        this.viral = this.viral + viralToAdd;
+	        this.yava = parseFloat(this.yava % 8);
+	    }
+	    if (this.viral >= 24) {
+	        var koalToAdd = Math.floor(this.viral / 24);
+	        this.koal = this.koal + koalToAdd;
+	        this.viral = this.viral % 24;
+	    }
+        if (negative)
+        {
+            reverseSign();
+        }
 	}
 	this.clone = function() {
 		return new VasthuUnit(this.koal, this.viral, this.yava);
 	}
 	this.toString = function() {
-		return this.koal + " koal " + this.viral + " viral " + this.yava + " yava";
+		return this.koal + " koal " + this.viral + " viral " + parseFloat(this.yava).toFixed(2) + " yava";
+	}
+	this.reverseSign = function () {
+	    this.cm = this.cm * -1;
+	    this.koal = this.koal * -1;
+	    this.viral = this.viral * -1;
+	    this.yava = this.yava * -1;
+	}
+	this.updateCMValue = function () {
+	    if (arguments.length > 0) {
+	        this.cm = arguments[0];
+	    }
+        var reminder;
+		this.koal = Math.floor(cm / 72);
+		reminder = cm % 72;
+		this.viral = Math.floor(reminder / 3);
+		reminder = reminder % 3;
+		this.yava = reminder / (3 / 8);
+		this.yava = parseFloat(this.yava);
+	}
+	this.isNegative = function () {
+	    return this.koal < 0 && this.viral < 0 && this.yava < 0;
 	}
 }
--- www/Script/events.js
+++ www/Script/events.js
@@ -10,21 +10,14 @@ var app = angular.module('VasthuUnitApp', []);
 app.globalSettings = {};
 app.globalSettings.maxNumberLength = 10;
 app.controller('convertPageController', ['$scope', function($scope) {
-	$scope.unitObjectForCMToV = new VasthuUnit(0);
-	$scope.unitObjectForVToCM = new VasthuUnit(0);
+	$scope.unitObjectForCMToV = new VasthuUnit(0, 0, 0);
+	$scope.unitObjectForVToCM = new VasthuUnit(0, 0, 0);
 	$scope.pageProperties = {};
 	$scope.pageProperties.convertDirection = 1;
-	$scope.convertedFromCM = function() {
-		var value = $scope.unitObjectForCMToV.getVasthuUnit();
-		return value.koal + " koal " + value.viral + " viral " + value.yava + " yava";
-	}
-	$scope.convertedFromVasthuUnit = function() {
-		if ($scope.pageProperties.autoRearrange == true) {
-			$scope.unitObjectForVToCM.rearrange();
-		}
-		return $scope.unitObjectForVToCM.getCM() + "cm";
-	}
+	$scope.convertedFromCM = "";
+	$scope.convertedFromVasthuUnit = "";
 	$scope.getActiveConvertDirectionCSS = function() {
+		console.log(arguments.callee);
 		if ($scope.pageProperties.convertDirection == 1) {
 			return "ui-btn-active";
 		} else {
@@ -32,81 +25,146 @@ app.controller('convertPageController', ['$scope', function($scope) {
 		}
 	}
 	$scope.pageProperties.autoRearrange = true;
-}]);
-app.controller("addScreenController", function($scope) {
-	$scope.additionItems = [];
-	$scope.additionItems[0] = {};
-	$scope.additionItems[0].unit = new VasthuUnit(0, 0, 0);
-	$scope.additionItems[0].add = true;
-	$scope.additionItems[1] = {};
-	$scope.additionItems[1].unit = new VasthuUnit(0, 0, 0);
-	$scope.additionItems[1].add = true;
-	$scope.addNew = function() {
-		$scope.additionItems.push({
-			add: true,
-			unit: new VasthuUnit(0, 0, 0)
-		});
-	}
-	$scope.remove = function() {
-		$scope.additionItems.pop();
-	}
-	$scope.getSum = function() {
-		var sum = new VasthuUnit(0, 0, 0);
-		var unitObject;
-		$("#page_addScreen_itemList").listview().trigger('create');
-		$("#page_addScreen_itemList").listview('refresh');
-		for (unitObject in $scope.additionItems) {
-			$scope.additionItems[unitObject].unit.rearrange();
-			if ($scope.additionItems[unitObject].add == true) {
-				sum.add($scope.additionItems[unitObject].unit);
-			} else {
-				sum.subtract($scope.additionItems[unitObject].unit);
-			}
+	var unitObjectForVToCM_watch_koal = $scope.$watch("unitObjectForVToCM.koal", function(newVal, oldVal) {
+		if (newVal != oldVal) {
+			var v = new VasthuUnit(newVal, $scope.unitObjectForVToCM.viral, $scope.unitObjectForVToCM.yava);
+			$scope.convertedFromVasthuUnit = v.getCM().toFixed(2) + " cm";
 		}
-		return sum;
-	}
-});
-app.controller("GeometricalCalculationsController", function($scope) {
+	});
+	var unitObjectForVToCM_watch_viral = $scope.$watch("unitObjectForVToCM.viral", function(newVal, oldVal) {
+		if (newVal != oldVal) {
+			var v = new VasthuUnit($scope.unitObjectForVToCM.koal, newVal, $scope.unitObjectForVToCM.yava);
+			$scope.convertedFromVasthuUnit = v.getCM().toFixed(2) + " cm";
+		}
+	});
+	var unitObjectForVToCM_watch_yava = $scope.$watch("unitObjectForVToCM.yava", function(newVal, oldVal) {
+		if (newVal != oldVal) {
+			var v = new VasthuUnit($scope.unitObjectForVToCM.koal, $scope.unitObjectForVToCM.viral, newVal);
+			$scope.convertedFromVasthuUnit = v.getCM().toFixed(2) + " cm";
+		}
+	});
+	var unitObjectForCMToV_watch_yava = $scope.$watch("unitObjectForCMToV.cm", function(newVal, oldVal) {
+		if (newVal != oldVal) {
+			var v = new VasthuUnit(newVal);
+			$scope.convertedFromCM = v.toString();
+		}
+	});
+}]);
+app.controller("addScreenController", ['$scope', function ($scope) {
+    $scope.additionItems = [];
+    $scope.additionItems[0] = {};
+    $scope.additionItems[0].unit = new VasthuUnit(0, 0, 0);
+    $scope.additionItems[0].add = true;
+    $scope.additionItems[1] = {};
+    $scope.additionItems[1].unit = new VasthuUnit(0, 0, 0);
+    $scope.additionItems[1].add = true;
+    $scope.addNew = function () {
+        console.log(arguments.callee);
+        $scope.additionItems.push({
+            add: true,
+            unit: new VasthuUnit(0, 0, 0)
+        });
+        $("#page_addScreen_itemList").listview().trigger('create');
+        $("#page_addScreen_itemList").listview('refresh');
+    }
+    $scope.remove = function () {
+        console.log(arguments.callee);
+        $scope.additionItems.pop();
+        $("#page_addScreen_itemList").listview().trigger('create');
+        $("#page_addScreen_itemList").listview('refresh');
+    }
+    $scope.getSum = new VasthuUnit(0);
+    var additionItems_watch = $scope.$watch("additionItems", function (newVal, oldVal) {
+        var sum = new VasthuUnit(0, 0, 0);
+        var i;
+        for (i in newVal) {
+            newVal[i].unit.rearrange();
+            if (newVal[i].add == true) {
+                sum.add(newVal[i].unit);
+            } else {
+                sum.subtract(newVal[i].unit);
+            }
+            sum.rearrange();
+            $scope.getSum = sum;
+        }
+    }, true);
+} ]);
+app.controller("GeometricalCalculationsController", ['$scope', function($scope) {
 	$scope.unitObject = new VasthuUnit(0, 0, 0);
 	$scope.divident = 0;
-	$scope.x2 = function() {
+	$scope.x2 = "";
+	$scope.x4 = "";
+	$scope.perimeter = "";
+	$scope.division = "";
+	var unitObject_watch = $scope.$watch("unitObject", function(newVal, oldVal) {
 		var t = $scope.unitObject.clone();
 		t.multiply(2);
-		return t.koal + " koal " + t.viral + " viral " + t.yava + " yava";
-	}
-	$scope.x4 = function() {
-		var t = $scope.unitObject.clone();
+		$scope.x2 = t.toString();
+		t.multiply(3.14);
+		$scope.perimeter = t.toString();
+		t = $scope.unitObject.clone();
 		t.multiply(4);
-		return t.koal + " koal " + t.viral + " viral " + t.yava + " yava";
-	}
-	$scope.perimeter = function() {
-		var t = $scope.unitObject.clone();
-		var pi = 3.14;
-		t.multiply(2 * pi);
-		return t.koal + " koal " + t.viral + " viral " + t.yava + " yava";
-	}
-	$scope.division = function() {
-		$scope.unitObject.rearrange();
-		var t = $scope.unitObject.clone();
+		$scope.x4 = t.toString();
 		if ($scope.divident != 0) {
+			t = $scope.unitObject.clone();
 			t.divide($scope.divident);
-			return t.toString();
+			$scope.division = t.toString();
 		} else {
-			return "";
+			$scope.division = "";
 		}
-	}
-});
-$(function () {
-    Test();
-    $("input[type='number']").each(function (i) {
-        $(this).keydown(function (e) {
-            var val = $(this).val();
-            if (val.length >= app.globalSettings.maxNumberLength) {
-                $(this).val(val.slice(0, app.globalSettings.maxNumberLength));
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        });
-        this.step = "any";
-    });
+	}, true);
+	var divident_watch = $scope.$watch("divident", function(newVal, oldVal) {
+		if (newVal != oldVal) {
+			if (newVal != 0) {
+				var t = $scope.unitObject.clone();
+				t.divide(newVal);
+				$scope.division = t.toString();
+			} else {
+				$scope.division = "";
+			}
+		}
+	});
+}]);
+$(function() {
+	Test();
+	$("input[type='number']").each(function(i) {
+		$(this).keydown(function(e) {
+			var val = $(this).val();
+			if (val.length >= app.globalSettings.maxNumberLength) {
+				$(this).val(val.slice(0, app.globalSettings.maxNumberLength));
+				e.preventDefault();
+				e.stopPropagation();
+			}
+		});
+		this.step = "any";
+	});
+	this.step = "any";
+	$("#app_main_body").pagecontainer({
+		beforeshow: function(event, ui) {
+			var page = $("body").pagecontainer("getActivePage")[0].id;
+			console.log(page);
+			if (page == "page_homeScreen") {
+				//Home screen stuff.
+			} else if (page == "page_convertPage") {
+				//Convert screen stuff.
+			} else if (page == "page_addScreen") {
+				//Add screen stuff.
+			} else if (page == "page_geometricalCalulations") {
+				//Geometrical Calculation screen stuff.
+			}
+		},
+		hide: function(event, ui) {
+			var page = $("body").pagecontainer("getActivePage")[0].id;
+			console.log(page);
+			if (page == "page_homeScreen") {
+				//Home screen stuff.
+			} else if (page == "page_convertPage") {
+				//Convert screen stuff.
+			} else if (page == "page_addScreen") {
+				//Add screen stuff.
+			} else if (page == "page_geometricalCalulations") {
+				//Geometrical Calculation screen stuff.
+			}
+		}
+	});
 });
